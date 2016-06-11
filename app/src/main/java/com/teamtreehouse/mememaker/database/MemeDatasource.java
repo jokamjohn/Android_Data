5671822007ecfbf67d8641039513e07bc3e84639
johnkagga <johnkagga@gmail.com>
2016-06-11 21:15:36 +0300

    Database transaction

--- app/src/main/java/com/teamtreehouse/mememaker/database/MemeDatasource.java
+++ app/src/main/java/com/teamtreehouse/mememaker/database/MemeDatasource.java
@@ -3,6 +3,8 @@ package com.teamtreehouse.mememaker.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.teamtreehouse.mememaker.models.Meme;
+
 /**
  * Created by Evan Anger on 8/17/14.
  */
@@ -16,5 +18,32 @@ public class MemeDatasource {
         mMemeSqlLiteHelper = new MemeSQLiteHelper(context);
         SQLiteDatabase database = mMemeSqlLiteHelper.getReadableDatabase();//Enable the database to set up the schema
         database.close();
+    }
+
+    /**
+     * Open the database connection.
+     *
+     * @return SQLiteDatabase
+     */
+    private SQLiteDatabase open()
+    {
+        return mMemeSqlLiteHelper.getWritableDatabase();
+    }
+
+    private void close (SQLiteDatabase database)
+    {
+        database.close();
+    }
+
+    public void create (Meme meme)
+    {
+        SQLiteDatabase database = open();
+        database.beginTransaction();
+        //Implementation logic
+
+
+        database.setTransactionSuccessful();
+        database.endTransaction();
+        close(database);
     }
 }
